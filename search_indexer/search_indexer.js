98780770835f24f23228ef7747de5386b5f76a09
Simon Jodet <simon@jodet.com>
2011-09-09 09:01:30 +0200

    Switching to full text search / Will use words for autocompletion
    Signed-off-by: Simon Jodet <simon@jodet.com>

--- search_indexer/search_indexer.js
+++ search_indexer/search_indexer.js
@@ -12,8 +12,12 @@ var postKeywords = {
   'iteratePosts': function()
   {
     var wholePost = postKeywords.posts[0].match(/---POST_URL_S---(.*)---POST_URL_E---/);
-    postKeywords.post = wholePost[1];
-    postKeywords.words = wholePost.input.toLowerCase().replace(/---POST_URL_S---.*---POST_URL_E---/i,'').replace(/<\w+(\s+("[^"]*"|'[^']*'|[^>])+)?>|<\/\w+>/gi, ' ').replace(/\n/gi, ' ').replace(/[^A-Za-z0-9]/gi,' ').replace(/\b[A-Za-z0-9]{1,2}\b/gi,' ').replace(/\s{2,}/gi,' ').replace(/^\s+/, '').replace(/\s+$/, '').split(' ');
+    postKeywords.post = {
+      'title': wholePost[1],
+      'content': wholePost.input.replace(/---POST_URL_S---.*---POST_URL_E---/i,'').replace(/<\w+(\s+("[^"]*"|'[^']*'|[^>])+)?>|<\/\w+>/gi, '').replace(/\n/gi, ' ').replace(/\s{2,}/gi,' ')
+    };
+
+    postKeywords.words = wholePost.input.toLowerCase().replace(/---POST_URL_S---.*---POST_URL_E---/i,'').replace(/<\w+(\s+("[^"]*"|'[^']*'|[^>])+)?>|<\/\w+>/gi, '').replace(/\n/gi, ' ').replace(/[^A-Za-z0-9]/gi,' ').replace(/\b[A-Za-z0-9]{1,2}\b/gi,' ').replace(/\s{2,}/gi,' ').replace(/^\s+/, '').replace(/\s+$/, '').split(' ');
     postKeywords.posts.shift();
     if(postKeywords.posts.length > 1){
       postKeywords.callback = postKeywords.iteratePosts;
@@ -24,18 +28,18 @@ var postKeywords = {
     postKeywords.createPost();
   },
   'createPost': function(){
-    var sql = 'INSERT INTO posts (post) VALUES ("' + postKeywords.post + '")';
+    var sql = 'INSERT INTO posts (post, content) VALUES ("' + postKeywords.post.title + '", "' + postKeywords.post.content.replace(/"/, '') + '")';
     db.execute( sql, [],
       function (error, rows) {
         if (error && error.message == 'constraint failed') {
-          var sql = "SELECT id FROM posts WHERE post = '" + postKeywords.post + "';";
+          var sql = "SELECT id FROM posts WHERE post = '" + postKeywords.post.title + "';";
           db.execute( sql, [],
             function (error, rows) {
               if (error) {
                 throw error;
               }
               postKeywords.post_id = rows[0]['id'];
-              // console.log('post already existing at ', postKeywords.post_id);
+              console.log('post already existing at ', postKeywords.post_id);
               postKeywords.insertWords();
             }
           );
@@ -48,7 +52,7 @@ var postKeywords = {
                 throw error;
               }
               postKeywords.post_id = rows[0]['last_insert_rowid()'];
-              // console.log('post inserted at id', postKeywords.post_id);
+              console.log('post inserted at id', postKeywords.post_id);
               postKeywords.insertWords();
             }
           );
@@ -69,97 +73,84 @@ var postKeywords = {
               if (error) {
                 throw error;
               }
-              // console.log('word', word, 'already existing at', rows[0]['id']);
-              var sql = 'INSERT INTO words_posts (word_id, post_id) VALUES (' + rows[0]['id'] + ', ' + postKeywords.post_id + ')';
-              db.execute( sql, [], function(error, rows){
-                  if(postKeywords.words.length > 1){
-                    postKeywords.insertWords();
-                  }
-                  else{
-                    postKeywords.callback();
-                  }
-                }
-              );
-            }
-          );
-        }
-        else {
-          var sql = 'SELECT last_insert_rowid() FROM words;';
-          db.execute( sql, [],
-            function (error, rows) {
-              if (error) {
-                throw error;
+              console.log('word', word, 'already existing at', rows[0]['id']);
+              if(postKeywords.words.length > 1){
+                postKeywords.insertWords();
               }
-              // console.log('word', word, 'inserted at id', rows[0]['last_insert_rowid()']);
-              var sql = 'INSERT INTO words_posts (word_id, post_id) VALUES (' + rows[0]['last_insert_rowid()'] + ', ' + postKeywords.post_id + ')';
-              db.execute( sql, [], function(error, rows){
-                  if(postKeywords.words.length > 1){
-                    postKeywords.insertWords();
-                  }
-                  else{
-                    postKeywords.callback();
-                  }
-                }
-              );
-            }
-          );
-        }
-      }
-    );
-  }
-};
-
-db.open("search.db", function (error) {
-  if (error) {
-    // console.log(error);
-    throw error;
-  }
-
-  var sql = 'PRAGMA foreign_keys = ON;';
-  db.execute( sql, [],
-    function (error, rows) {
-      if (error) {
-        throw error;
-      }
-      // console.log('Foreign keys activated.');
-
-      var sql = 'CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, "post" TEXT NOT NULL UNIQUE);';
-      db.execute( sql, [],
-        function (error, rows) {
-          if (error) {
-            throw error;
+              else{
+                postKeywords.callback();
+              }
           }
-          // console.log("posts table created.");
-          var sql = 'CREATE TABLE IF NOT EXISTS "words" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "word" TEXT NOT NULL UNIQUE);';
-          db.execute( sql, [],
-            function (error, rows) {
-              if (error) {
-                throw error;
-              }
-              // console.log("words table created.");
-              var sql = 'CREATE TABLE IF NOT EXISTS "words_posts" ("word_id" INTEGER NOT NULL REFERENCES words(id) ON UPDATE CASCADE ON DELETE CASCADE, "post_id" INTEGER NOT NULL REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE, PRIMARY KEY (word_id, post_id));';
-              db.execute( sql, [],
-                function (error, rows) {
-                  if (error) {
-                    throw error;
-                  }
-                  // console.log("words_posts table created.");
-                  fs.readFile('../_site/search/keywords.html', function (err, data) {
-                    if (err) {
-                      throw err;
-                    }
-                    postKeywords.posts = data.toString().split('---POST_SPLIT---');
-                    postKeywords.finalCallback = function(){
-                      console.log("I'm done!");
-                    };
-                    postKeywords.iteratePosts();
-                  });
-                }
-              );
+        );
+      }
+      else {
+        var sql = 'SELECT last_insert_rowid() FROM words;';
+        db.execute( sql, [],
+          function (error, rows) {
+            if (error) {
+              throw error;
             }
-          );
+            console.log('word', word, 'inserted at id', rows[0]['last_insert_rowid()']);
+            if(postKeywords.words.length > 1){
+              postKeywords.insertWords();
+            }
+            else{
+              postKeywords.callback();
+            }
         }
       );
     }
-  );
+  }
+);
+}
+};
+fs.unlink('search.db', function(error){
+  if (error) {
+    console.log(error);
+  }
+  console.log('Database erased.')
+  db.open('search.db', function (error) {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+
+    var sql = 'PRAGMA foreign_keys = ON;';
+    db.execute( sql, [],
+      function (error, rows) {
+        if (error) {
+          throw error;
+        }
+        console.log('Foreign keys activated.');
+        var sql = 'CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, "post" TEXT NOT NULL UNIQUE, "content" TEXT NOT NULL UNIQUE);';
+        db.execute( sql, [],
+          function (error, rows) {
+            if (error) {
+              throw error;
+            }
+            console.log("posts table created.");
+            var sql = 'CREATE TABLE IF NOT EXISTS "words" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "word" TEXT NOT NULL UNIQUE);';
+            db.execute( sql, [],
+              function (error, rows) {
+                if (error) {
+                  throw error;
+                }
+                console.log("words table created.");
+                fs.readFile('../_site/search/keywords.html', function (err, data) {
+                  if (err) {
+                    throw err;
+                  }
+                  postKeywords.posts = data.toString().split('---POST_SPLIT---');
+                  postKeywords.finalCallback = function(){
+                    console.log("I'm done!");
+                  };
+                  postKeywords.iteratePosts();
+                });
+              }
+            );
+          }
+        );
+      }
+    );
+  });
 });
