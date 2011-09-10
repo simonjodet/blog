039a7477bea44932a6a97de12bbafed183d40c57
Simon Jodet <simon@jodet.com>
2011-09-10 20:00:44 +0200

    Enhancing search
    Signed-off-by: Simon Jodet <simon@jodet.com>

--- search/search.js
+++ search/search.js
@@ -3,28 +3,51 @@ var http = require('http'), querystring = require('querystring'), sqlite = requi
 var db = new sqlite.Database();
 
 http.createServer(function (req, res) {
+  // Open the database
   db.open('../search_indexer/search.db', function (error) {
     if (error) {
       console.log(error);
-      throw error;
+      // Return empty results
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(new Array())+'\n');
     }
+    // Handle the search query
     var queryParameters = querystring.parse(req.url);
-    var searchString = '';
+    var searchStrings = [];
     for (var key in queryParameters) {
       if (queryParameters.hasOwnProperty(key) && key.replace(/[^A-Za-z0-9 ]/gi,'') == 'search') {
-        searchString = queryParameters[key].replace(/[^A-Za-z0-9 ]/gi,'');
+        searchStrings = queryParameters[key].replace(/[^A-Za-z0-9 ]/gi,'').split(' ');
       }
     }
-    var sql = 'SELECT post FROM posts WHERE content LIKE "%' + searchString + '%";';
-    db.execute( sql, [],
-      function (error, rows) {
-        if (error) {
-          throw error;
-        }
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(JSON.stringify(rows)+'\n');
+    // Build the SQL query
+    var additionnalStrings = '';
+    if(searchStrings.length > 1) {
+      for(var i=1; i < searchStrings.length; i++) {
+        additionnalStrings = additionnalStrings + ' AND content LIKE "%' + searchStrings[i] + '%"';
       }
-    );
+    }
+    if(searchStrings.length > 0 && searchStrings[0] != '') {
+      var sql = 'SELECT DISTINCT post, content FROM posts WHERE content LIKE "%' + searchStrings[0] + '%"' + additionnalStrings + ';';
+      console.log(sql);
+      db.execute( sql, [],
+        function (error, rows) {
+          if (error) {
+            console.log(error);
+            // Return empty results
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(JSON.stringify(new Array())+'\n');
+          }
+          // Return results
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(JSON.stringify(rows)+'\n');
+        }
+      );
+    }
+    else {
+      // Return empty results
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(new Array())+'\n');
+    }
   }
 );
 }).listen(1337, "127.0.0.1");
