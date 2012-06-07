8dbd75b6f513394af2f7dd55a984812b3c17983c
Simon Jodet <simon.jodet@f-secure.com>
2012-06-07 21:34:56 +0200

    Adding search engine in PHP

---
+++ search/search.php
@@ -0,0 +1,3 @@
+<?php
+$db_file = __DIR__.'/search.sqlite';
+$db = sqlite_open($db_file);
---
+++ search_indexer/search_indexer.php
@@ -0,0 +1,31 @@
+<?php
+$db_file = __DIR__.'/search.sqlite';
+if(file_exists($db_file)){
+    unlink($db_file);
+}
+$db = sqlite_open($db_file);
+$sql = 'CREATE TABLE "posts" ("id" INTEGER AUTOINCREMENT PRIMARY KEY NOT NULL UNIQUE, "post" TEXT NOT NULL, "title" TEXT NOT NULL, "date" TEXT NOT NULL, "content" TEXT NOT NULL);';
+sqlite_query($db, $sql);
+$keywords = file_get_contents(__DIR__.'/../_site/search/keywords.html');
+$raw_posts = explode('---POST_SPLIT---', $keywords);
+unset($raw_posts[count($raw_posts)-1]);
+$posts = array();
+foreach($raw_posts as $raw_post){
+    $post = array();
+    preg_match('/.*---POST_URL_S---(.*)---POST_URL_E---.*/', $raw_post, $matches);
+    $post['post'] = $matches[1];
+    preg_match('/.*---POST_DATE_S---(.*)---POST_DATE_E---.*/', $raw_post, $matches);
+    $post['date'] = $matches[1];
+    preg_match('/.*---POST_TITLE_S---(.*)---POST_TITLE_E---.*/', $raw_post, $matches);
+    $post['title'] = $matches[1];
+    $content = preg_replace('/---POST_URL_S---.*---POST_URL_E---/i', '', $raw_post);
+    $content = preg_replace('/---POST_DATE_S---.*---POST_DATE_E---/i', '', $content);
+    $content = preg_replace('/---POST_TITLE_S---.*---POST_TITLE_E---/i', '', $content);
+    preg_match_all('/\b[a-z0-9]+\b/i', $content, $matches);
+    $content = implode(' ', $matches[0]);
+    
+    $post['content'] = $content;
+    sqlite_query($db, 'INSERT INTO posts (post, title, date, content) VALUES ("'.$post['post'].'","'.$post['title'].'","'.$post['date'].'","'.$post['content'].'")');
+    $posts[] = $post;
+}
+sqlite_close($db);
