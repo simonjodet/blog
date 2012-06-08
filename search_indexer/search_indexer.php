bb4725da56feeeed30206c0e3f29c74024341c5a
Simon Jodet <simon.jodet@f-secure.com>
2012-06-08 13:56:33 +0200

    Fixing PHP search

--- search_indexer/search_indexer.php
+++ search_indexer/search_indexer.php
@@ -3,9 +3,9 @@ $db_file = __DIR__.'/search.sqlite';
 if(file_exists($db_file)){
     unlink($db_file);
 }
-$db = sqlite_open($db_file);
-$sql = 'CREATE TABLE "posts" ("id" INTEGER AUTOINCREMENT PRIMARY KEY NOT NULL UNIQUE, "post" TEXT NOT NULL, "title" TEXT NOT NULL, "date" TEXT NOT NULL, "content" TEXT NOT NULL);';
-sqlite_query($db, $sql);
+$db = new PDO('sqlite:'.$db_file);
+$sql = 'CREATE TABLE posts (id INTEGER PRIMARY KEY NOT NULL, post TEXT NOT NULL, title TEXT NOT NULL, date TEXT NOT NULL, content TEXT NOT NULL);';
+$db->exec($sql);
 $keywords = file_get_contents(__DIR__.'/../_site/search/keywords.html');
 $raw_posts = explode('---POST_SPLIT---', $keywords);
 unset($raw_posts[count($raw_posts)-1]);
@@ -18,14 +18,15 @@ foreach($raw_posts as $raw_post){
     $post['date'] = $matches[1];
     preg_match('/.*---POST_TITLE_S---(.*)---POST_TITLE_E---.*/', $raw_post, $matches);
     $post['title'] = $matches[1];
-    $content = preg_replace('/---POST_URL_S---.*---POST_URL_E---/i', '', $raw_post);
-    $content = preg_replace('/---POST_DATE_S---.*---POST_DATE_E---/i', '', $content);
-    $content = preg_replace('/---POST_TITLE_S---.*---POST_TITLE_E---/i', '', $content);
+    $content = preg_replace('/---POST_URL_S---.*---POST_URL_E---/i', ' ', $raw_post);
+    $content = preg_replace('/---POST_DATE_S---.*---POST_DATE_E---/i', ' ', $content);
+    $content = preg_replace('/---POST_TITLE_S---.*---POST_TITLE_E---/i', ' ', $content);
+    $content = strip_tags($content);
     preg_match_all('/\b[a-z0-9]+\b/i', $content, $matches);
     $content = implode(' ', $matches[0]);
     
     $post['content'] = $content;
-    sqlite_query($db, 'INSERT INTO posts (post, title, date, content) VALUES ("'.$post['post'].'","'.$post['title'].'","'.$post['date'].'","'.$post['content'].'")');
+    $db->exec('INSERT INTO posts (post, title, date, content) VALUES ("'.$post['post'].'","'.$post['title'].'","'.$post['date'].'","'.$post['content'].'")');
     $posts[] = $post;
 }
-sqlite_close($db);
+$db = null;
