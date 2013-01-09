229b662ae8b74caba8186c91deb55ab1e20db83c
Simon Jodet <simon@jodet.com>
2013-01-09 10:23:20 +0100

    Click on tags triggers a search among posts's tags

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ jQuery(function ($) {
     }
   });
 
+  //Search and tags
   var resetSearch = function () {
     $('.search_result').detach();
     $('#no_result_message').hide();
@@ -20,48 +21,59 @@ jQuery(function ($) {
     resetSearch();
   });
 
-  $('#search_btn').click(function (event) {
-    event.preventDefault();
+  var displaySearchResults = function (data) {
+    $('#searching_message').hide();
+    if (!$.isArray(data) || data.length < 1) {
+      $('#no_result_message').show();
+    }
+    else {
+      $('#search_container').show();
+      $.each(data, function (index, post) {
+        var search_result = $('#search_result_template').clone().insertAfter($('#search_result_template')).addClass('search_result').removeAttr('id').show();
+        search_result.find('.search_post_link').html(post.title).attr('href', "/" + post.location);
+        search_result.find('.search_post_date').html(post.date);
+        search_result.find('.search_post_content').html(post.content);
+      });
+    }
+  };
+
+  var JsonSearch = function (search, criteria) {
+    var search = search;
+    var criteria = criteria;
+    $.getJSON("/search_db.json", function (json) {
+      jsonCallback(json, search);
+    });
+
+    function jsonCallback(json, search) {
+      var pages = json.pages;
+      var stringData = '';
+      var results = jQuery.grep(pages, function (page, index) {
+        for (var i = 0; i < criteria.length; i += 1) {
+          if ($.isArray(page[criteria[i]])) {
+            stringData = page[criteria[i]].join(',').toLowerCase();
+          }
+          else {
+            stringData = page[criteria[i]].toLowerCase();
+          }
+          if (stringData.indexOf(search.toLowerCase()) != -1) {
+            return true;
+          }
+        }
+      });
+      displaySearchResults(results);
+    }
+  };
+
+  var initiateSearch = function () {
     $('#content_container').hide();
     $('#search_container').show();
     resetSearch();
+  };
 
-    var displaySearchResults = function (data) {
-      $('#searching_message').hide();
-      if (!$.isArray(data) || data.length < 1) {
-        $('#no_result_message').show();
-      }
-      else {
-        $('#search_container').show();
-        $.each(data, function (index, post) {
-          var search_result = $('#search_result_template').clone().insertAfter($('#search_result_template')).addClass('search_result').removeAttr('id').show();
-          search_result.find('.search_post_link').html(post.title).attr('href', "/" + post.location);
-          search_result.find('.search_post_date').html(post.date);
-          search_result.find('.search_post_content').html(post.content);
-        });
-      }
-    };
-
-    var JsonSearch = function (search) {
-      var search = search;
-      $.getJSON("search_db.json", function (json) {
-        jsonCallback(json, search);
-      });
-
-      function jsonCallback(json, search) {
-        var pages = json.pages;
-        var results = jQuery.grep(pages, function (page, index) {
-          if (page.title.toLowerCase().indexOf(search.toLowerCase()) != -1) {
-            return true;
-          }
-          if (page.content.toLowerCase().indexOf(search.toLowerCase()) != -1) {
-            return true;
-          }
-        });
-        displaySearchResults(results);
-      }
-    };
-    JsonSearch($('#search_field').attr('value'));
+  $('#search_btn').click(function (event) {
+    event.preventDefault();
+    initiateSearch();
+    JsonSearch($('#search_field').attr('value'), ['title', 'content']);
 
   });
 
@@ -72,6 +84,18 @@ jQuery(function ($) {
     }
   });
 
+  var attachTagSearch = function () {
+    $('.post_tag').each(function (index, element) {
+      $(element).off("click.search");
+      $(element).on("click.search", function (event) {
+        event.preventDefault();
+        initiateSearch();
+        JsonSearch(element.innerText, ['tags']);
+      });
+    });
+  };
+
+  //Pagination
   if ($('#pagination_btn').length == 1) {
     var isLoadingNewPage = false;
     var drawNewPage = function (page) {
@@ -80,7 +104,7 @@ jQuery(function ($) {
       clone.children('div.well').children('div.page-header').children('h3').children('a')[0].innerHTML = page.title;
       var subtitle = clone.children('div.well').children('div.page-header').children('h3').children('small')[0].innerHTML;
       subtitle = subtitle.replace("__page.date__", page.date);
-      subtitle = subtitle.replace("__page.tags__", page.tags.join(', '));
+      subtitle = subtitle.replace("__page.tags__", '<span class="post_tag">' + page.tags.join('</span>, <span class="post_tag">')) + '</span>';
       clone.children('div.well').children('div.page-header').children('h3').children('small')[0].innerHTML = subtitle;
       clone.children('div.well').children('div.post-content')[0].innerHTML = page.content;
       clone.css('display', 'block');
@@ -106,6 +130,7 @@ jQuery(function ($) {
               $('#pagination_btn').css('display', 'none');
               $(window).off('scroll', infiniteScrolling);
             }
+            attachTagSearch();
             prettyPrint();
             isLoadingNewPage = false;
             window.clearTimeout(autoreset);
@@ -128,5 +153,6 @@ jQuery(function ($) {
     $(window).on('scroll', infiniteScrolling);
   }
 
+  //Pretty print of code blocks
   prettyPrint();
 });
