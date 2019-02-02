3423b3a98a024450915c20936aca236acb3eba89
Simon Jodet <simon@stoic.com>
2019-02-02 11:00:36 +0100

    Listing posts

---
+++ src/components/posts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <template v-for = "post of posts">
+      <div v-html="post"></div>
+    </template>
+  </div>
+</template>
+
+<script>
+import posts from '../assets/posts.json';
+import markdown from 'markdown';
+
+export default {
+  name: 'posts',
+
+  computed: {
+    posts() {
+      const postContents = [];
+      for (const post of posts) {
+        postContents.push(markdown.parse(post.content));
+      }
+
+      return postContents;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <posts></posts>
+</template>
+
+<script>
+// @ is an alias to /src
+import posts from "@/components/posts.vue";
+
+export default {
+  name: "home",
+  components: {
+    posts
+  }
+};
+</script>
