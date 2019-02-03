7430ecac6e20d7ca1402628965852f6cc0f87430
Simon Jodet <simon@stoic.com>
2019-02-03 10:35:15 +0100

    Footer

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,14 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+    <v-footer height="auto" color="primary lighten-1">
+      <v-layout justify-center row wrap>
+        <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
+          &copy;2019 —
+          <strong>Simon Jodet</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
