122e0dca3ba36d391a55637aac7403d3bae37b47
Simon Jodet <simon@jodet.com>
2019-04-01 14:10:55 +0200

    Cleanup

--- src/router.js
+++ src/router.js
@@ -55,8 +55,7 @@ export default new Router({
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
-    } else {
-      return { x: 0, y: 0 };
     }
+    return { x: 0, y: 0 };
   }
 });
